00b329c0953116c7a3be45951299d0238719d5e5
Brian <byamagata@gmail.com>
2021-11-03 20:39:53 -0700

    Create a database instance.

--- app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -1,10 +1,15 @@
 package com.example.inventory.data
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface  ItemDao {
+interface ItemDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(item: Item)
 
