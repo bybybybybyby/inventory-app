f6719f662ae555b7dc2230150ba9addb341515e5
Brian <byamagata@gmail.com>
2021-11-21 20:56:59 -0800

    Read and display the entity details on the Item Details screen.

--- app/src/main/java/com/example/inventory/AddItemFragment.kt
+++ app/src/main/java/com/example/inventory/AddItemFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.inventory.data.Item
 import com.example.inventory.databinding.FragmentAddItemBinding
 
 /**
--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -3,6 +3,7 @@ package com.example.inventory.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.NumberFormat
 
 
 /**
@@ -20,3 +21,5 @@ data class Item(
     val quantityInStock: Int
 )
 
+fun Item.getFormattedPrice(): String =
+    NumberFormat.getCurrencyInstance().format(itemPrice)
